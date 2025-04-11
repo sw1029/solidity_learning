d5250957bb61b6aae1b2dd0a02022ee61df665e9
sw1029 <yunsw62@gmail.com>
2025-04-12 00:46:53 +0900

    test contract

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,11 +5,24 @@ contract MyToken{
     string public name;
     string public symbol;
     uint8 public decimals; // 소수점 n자리까지 지원하겠다
-    
+
+    uint256 public totalSupply; // 발행량
+    mapping(address => uint256) public balanceOf; // 잔고를 확인하기 위한 mapping
+
     constructor(string memory _name,string memory _symbol,uint8 _decimal){
         //길이가 정해져 있지 않은 string의 경우 memory를 사용해야 한다
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+    }
+
+    function _totalSupply()  external view returns (uint256) {
+        return totalSupply;
+    }
+    function _balanceOf(address owner) external view returns (uint256) {
+        return balanceOf[owner];
+    }
+    function _name() external view returns (string memory) {
+        return name;
     }
 }
--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,15 +1,30 @@
 import hre from "hardhat";
 import { expect } from "chai";
+import { MyToken } from "../typechain-types";
 
 describe("mytoken deploy",() => {
-    it("deploy",async () => {
-        const MyTokenC = await hre.ethers.deployContract("MyToken",[
+    let MyTokenC:MyToken;
+    before("deploy",async () => {
+        MyTokenC = await hre.ethers.deployContract("MyToken",[
             "MyToken",
             "MT",
             18,
         ]);
-        expect(await MyTokenC.name()).to.equal("MyToken");
-        expect(await MyTokenC.symbol()).to.equal("MT");
-        expect(await MyTokenC.decimals()).to.equal(18);
+    });
+    it("should return name", async () => {
+       expect(await MyTokenC.name()).to.equal("MyToken"); 
+    });
+    it("should return symbol", async () => {
+         expect(await MyTokenC.symbol()).to.equal("MT"); 
+    });
+    it("should return decimals", async () => {
+       expect(await MyTokenC.decimals()).to.equal(18); 
+    });
+    it("should return 0 totalsupply", async () => {
+        expect(await MyTokenC.totalSupply()).to.equal(0); 
+    });
+    it("should return 0 balance", async () => {
+        const signers = await hre.ethers.getSigners();
+        expect(await MyTokenC.balanceOf(signers[0].address)).to.equal(0); 
     });
 });
