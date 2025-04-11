a8a5fa917185b24a59ee988acdf3b0047cb21f20
sw1029 <yunsw62@gmail.com>
2025-04-12 01:07:52 +0900

    minting

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -11,9 +11,23 @@ contract MyToken{
 
     constructor(string memory _name,string memory _symbol,uint8 _decimal){
         //길이가 정해져 있지 않은 string의 경우 memory를 사용해야 한다
+        //생성할 때 발행량을 정해줄 수 있다
+        //딱 한번 호출됨
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        _mint(1*10*uint256(decimals), msg.sender);// 1 token == 10^18 wei
+        //msg.sender는 이 계약을 배포한 사람의 주소
+
+    }
+
+    //transaction
+    //from, to, data, value, gas...
+
+    function _mint(uint256 amount, address owner) internal{
+        // 발행량을 1 증가시키고, 발행한 사람의 잔고를 1 증가시킨다
+        totalSupply += amount;
+        balanceOf[owner] += amount;
     }
 
     function _totalSupply()  external view returns (uint256) {
--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,9 +1,11 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { MyToken } from "../typechain-types";
+//import { HardhatEthersSigner } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("mytoken deploy",() => {
     let MyTokenC:MyToken;
+    //let signers: HardhatEthersSigner[];
     before("deploy",async () => {
         MyTokenC = await hre.ethers.deployContract("MyToken",[
             "MyToken",
@@ -21,10 +23,10 @@ describe("mytoken deploy",() => {
        expect(await MyTokenC.decimals()).to.equal(18); 
     });
     it("should return 0 totalsupply", async () => {
-        expect(await MyTokenC.totalSupply()).to.equal(0); 
+        expect(await MyTokenC.totalSupply()).to.equal(1n*10n**18n); 
     });
     it("should return 0 balance", async () => {
         const signers = await hre.ethers.getSigners();
-        expect(await MyTokenC.balanceOf(signers[0].address)).to.equal(0); 
+        expect(await MyTokenC.balanceOf(signers[0].address)).to.equal(1n*10n**18n); 
     });
 });
