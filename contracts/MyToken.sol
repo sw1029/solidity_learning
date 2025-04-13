424894004d69c940837612dcfdea6072a0b443aa
sw1029 <yunsw62@gmail.com>
2025-04-13 20:03:33 +0900

    transferform, approve

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 contract MyToken{
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 value);
     // 이벤트를 발생시키는 경우
     //indexed를 사용하면 검색이 가능하다
     //indexing의 의미 == topic에 데이터를 넣어달라
@@ -17,6 +18,10 @@ contract MyToken{
 
     uint256 public totalSupply; // 발행량
     mapping(address => uint256) public balanceOf; // 잔고를 확인하기 위한 mapping
+
+    mapping(address => mapping(address => uint256)) public allowance;
+    // approve를 통해서 허가된 사람에게 보내는 것
+    // mapping(주소 => mapping(주소 => 잔고))
 
     constructor(string memory _name,string memory _symbol,uint8 _decimal,uint256 _amount){
         //길이가 정해져 있지 않은 string의 경우 memory를 사용해야 한다
@@ -58,6 +63,28 @@ contract MyToken{
         emit Transfer(msg.sender, to, amount); // 이벤트 발생
     }
 
+    //approve == 허가. 잘못 쓰면 토큰 잃어버린다
+        //그래서 보통 중간에 rauter를 두고, rauter가 approve를 받는다
+    //transferFrom == 허가된 사람에게 보내는 것
+    //transferFrom은 approve가 되어야만 사용 가능하다
 
+    function approve(address spender, uint256 amount) external {
+        // spender에게 amount만큼 허가한다
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount); // 이벤트 발생
+        // spender가 msg.sender에게 허가를 받았다는 것을 알린다
+    }
+    function transferFrom(address from, address to, uint256 amount) external {
+        // from에게서 to에게 amount만큼 보내는 것
+        // spender가 from에게 허가를 받았는지 확인한다
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance"); // 허가량이 부족하다
+        allowance[from][msg.sender] -= amount; // 허가량을 차감한다
+        balanceOf[from] -= amount; // 보낸사람의 잔고에서 amount만큼 차감
+        balanceOf[to] += amount; // 받는사람의 잔고에 amount만큼 추가
+
+        emit Transfer(from, to, amount); // 이벤트 발생
+    }
+    // approve를 통해서 허가된 사람에게 보내는 것
 
 }
