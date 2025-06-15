cdc39f62478b6d914478144f4547566a5a292230
sw1029 <yunsw62@gmail.com>
2025-06-16 03:13:33 +0900

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,12 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
   vyper: {
     version : "0.3.0",
+  },
+  networks: {
+    kairos: {
+      url: "https://public-en-kairos.node.kaia.io",
+      accounts: ["0xa07900d6508ed7c5a81c160c2e7f2542f0c57b151dd9b2f079cbbabb1af58d47"]
+    }
   }
 };
 
